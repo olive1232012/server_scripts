60fc3168b06728317804261538959d9abb53d036
olive1232012 <olive1232012@live.com>
2021-11-30 20:34:35 +0000

    Update cockpit-port-change.sh

--- cockpit-port-change.sh
+++ cockpit-port-change.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+# Makes the new directory
+sudo mkdir /etc/systemd/system/cockpit.socket.d
+
+# Moves the file to the correct folder
+sudo mv listen.conf /etc/systemd/system/cockpit.socket.d
+
+# Restarts the services
+sudo systemctl daemon-reload
+sudo systemctl restart cockpit.socket
